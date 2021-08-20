f5315e5397fd36487c934eff4a76918dc9478d04
Victor Simionato de Moura <victorsimionato3@gmail.com>
2021-08-20 15:07:24 -0300

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { RepositoryList } from './components/repositoryList';
+import './styles/global.scss';
+
+// ISSO É UM COMPONENTE
+// UM COMPONENTE POR ARQUIVO
+export function App() {
+    return (
+        <RepositoryList />
+    );
+}
